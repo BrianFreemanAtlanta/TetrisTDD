3072240483051cbf6dbd38cbdd7fc741e2bde8e5
Brian Freeman <freeman@carnegie.com>
2019-09-18 17:56:10 -0400

    refactor with switch statement

--- src/tetris.js
+++ src/tetris.js
@@ -17,13 +17,17 @@ var shapeI =
     [0,0,1,0]
 ]
 function getShape(pShape) {
-    if (pShape=='L') {
-        return shapeL;
-    } if (pShape=='I') {
-        return shapeI;
-    }
-    else {
-        return shapeT;
+    switch (pShape) {
+        case 'T':
+            return shapeT;
+            break;
+        case 'I':
+            return shapeI;
+        case 'L':
+            return shapeL;
+    
+        default:
+            break;
     }
     
 };
