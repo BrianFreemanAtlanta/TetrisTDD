567d6c1c6b364507694580adf6b1a428a5ae52c8
Brian Freeman <freeman@carnegie.com>
2019-09-18 17:44:37 -0400

    second test

--- spec/tetrisSpec.js
+++ spec/tetrisSpec.js
@@ -7,12 +7,21 @@ describe("FirstTest",function() {
 describe("Tetris", function(){
     //var getShape = require('../tetris').getShape;
     
-    it("GetShapeReturnsT", function(){
+    it("GetShapeTReturnsT", function(){
         expect(getShape('T')).toEqual(
             [
                 [0,0,0],
                 [1,1,1],
                 [0,1,0]
+            ]
+        )
+    })
+    it("GetShapeLReturnsL", function(){
+        expect(getShape('L')).toEqual(
+            [
+                [0,1,0],
+                [0,1,0],
+                [0,1,1]
             ]
         )
     })
--- src/tetris.js
+++ src/tetris.js
@@ -1,12 +1,22 @@
+var shapeT=[
+    [0,0,0],
+    [1,1,1],
+    [0,1,0]
+] ;
+var shapeL =         [
+    [0,1,0],
+    [0,1,0],
+    [0,1,1]
+] ;
+
+
 function getShape(pShape) {
-    if (pShape='T') {
-        
-        return [
-            [0,0,0],
-            [1,1,1],
-            [0,1,0]
-        ]
+    if (pShape=='L') {
+        return shapeL;
+    } else {
+        return shapeT;
     }
+    
 };
 
 // module.exports = getShape;
