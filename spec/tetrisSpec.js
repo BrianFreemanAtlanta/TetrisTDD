124e36d36999b632ede83660e1e46d387f200c85
Brian Freeman <freeman@carnegie.com>
2019-09-18 22:55:54 -0400

    file formatting

--- spec/tetrisSpec.js
+++ spec/tetrisSpec.js
@@ -70,4 +70,5 @@ describe("Tetris", function(){
             ]
         )
     })
+    
 });
--- src/tetris.js
+++ src/tetris.js
@@ -1,42 +1,48 @@
-var shapeT=[
-    [0,0,0],
-    [1,1,1],
-    [0,1,0]
-] ;
-var shapeL =         [
-    [0,1,0],
-    [0,1,0],
-    [0,1,1]
-] ;
-var shapeJ =         [
-    [0,1,0],
-    [0,1,0],
-    [1,1,0]
-] ;
-var shapeS =         [
-    [0,1,1],
-    [0,1,0],
-    [1,1,0]
-] ;
-var shapeZ =         [
-    [1,1,0],
-    [0,1,0],
-    [0,1,1]
-] ;
+var shapeT=         
+    [
+        [0,0,0],
+        [1,1,1],
+        [0,1,0]
+    ] ;
+var shapeL =         
+    [
+        [0,1,0],
+        [0,1,0],
+        [0,1,1]
+    ] ;
+var shapeJ =         
+    [
+        [0,1,0],
+        [0,1,0],
+        [1,1,0]
+    ] ;
+var shapeS =         
+    [
+        [0,1,1],
+        [0,1,0],
+        [1,1,0]
+    ] ;
+var shapeZ =         
+    [
+        [1,1,0],
+        [0,1,0],
+        [0,1,1]
+    ] ;
 
-var shapeI = 
-[
-    [0,0,1,0],
-    [0,0,1,0],
-    [0,0,1,0],
-    [0,0,1,0]
-]
+var shapeI =
+    [
+        [0,0,1,0],
+        [0,0,1,0],
+        [0,0,1,0],
+        [0,0,1,0]
+    ]
 var shapeO =
     [
         [1,1],
         [1,1]
     ]
-function getShape(pShape) {
+
+    function getShape(pShape) {
     switch (pShape) {
         case 'T':
             return shapeT;
@@ -53,7 +59,7 @@ function getShape(pShape) {
             return shapeS;
         case 'Z':
             return shapeZ;
-            default:
+        default:
             break;
     }
     
